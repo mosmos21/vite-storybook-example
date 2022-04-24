6fe9d216bd177a1c90a780f6235a34c76d0bcc09
mosmos21 <dev@mosmos21.net>
2022-04-24 15:58:06 +0900

    add SomeComponent

---
+++ src/components/SomeComponent.stories.tsx
@@ -0,0 +1,7 @@
+import { SomeComponent } from "./SomeComponent";
+
+export default {
+  component: SomeComponent
+}
+
+export const Default = {}
---
+++ src/components/SomeComponent.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+export const SomeComponent: FC = () => (
+  <div>
+    SomeComponent
+  </div>
+);
